bd6fc4fe80657686b8e19e5b6b35671c6730f2b0
Marlon020Victor <145376158+Marlon020Victor@users.noreply.github.com>
2024-12-19 17:14:47 -0300

    Robson(V.01)

---
+++ Rodrigo The Zombie/Assets/Scripts/MoveBasics.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBasics : MonoBehaviour
+{
+    
+    public float Forca;
+    public float Speed;
+    private Rigidbody2D rig;
+    private Animator anim;
+    public bool Nochao;
+    public bool Jumping;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D Player)
+    {
+        if (Player.gameObject.tag == "NoChao")
+        {
+            Nochao = true;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Jumping == false)
+        {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+            transform.position += movement * Speed;
+
+            float input = Input.GetAxis("Horizontal");
+
+            if (input > 0)
+            {
+                transform.eulerAngles = new Vector2(0f, 0f);
+                anim.SetBool("Walk", true);
+            }
+            else if (input < 0)
+            {
+                transform.eulerAngles = new Vector2(180f, 0f);
+            }
+            else if (Input.GetKey(KeyCode.F) && Jumping == false)
+            {
+                anim.SetBool("Walk", true);
+            }
+            
+        }
+        
+        if (Input.GetButtonDown("Jump") && Nochao && !Jumping)
+        {
+            rig.AddForce(Vector2.up * Forca, ForceMode2D.Impulse);
+            Nochao = false;
+            Jumping = true; 
+            anim.SetBool("Jump", true);
+        }
+        
+    }
+}
